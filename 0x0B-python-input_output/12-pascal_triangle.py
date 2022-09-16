db3734479199b9179bc06d7ed69525e5f0ecce76
Cynthia Amaji <cynthyok4luv@gmail.com>
2022-09-16 09:47:22 +0000

    pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+'''A module containing combinatorial mathematical functions.
+'''
 
 
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
-    Returns a list of lists of integers representing the triangle.
-    """
+    '''This generates a list of lists of integers representing the
+    Pascal’s triangle of a given integer.
+    Args:
+        n (int): The number of rows in the triangle.
+    Returns:
+        list: A list of integers containing the values of their
+        respective row in Pascal's triangle for the given number.
+    '''
+    res = []
     if n <= 0:
-        return []
-
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return 
+        return res
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            top_left = 1 if i == 0 else 0
+            top_right = 0
+            if (i > 0) and (j > 0):
+                top_left = res[i - 1][j - 1]
+            if (i > 0) and (j < len(res[i - 1])):
+                top_right = res[i - 1][j]
+            row.append(top_left + top_right)
+        res.append(row)
+    return res
