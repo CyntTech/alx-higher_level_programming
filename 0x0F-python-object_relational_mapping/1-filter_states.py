47177807f7c3a9c6e9992f3466be35a74b117de1
Cynthia Amaji <cynthyok4luv@gmail.com>
2022-10-09 20:41:43 +0000

    list of all states with an upper N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-listing of all states with a name starting with N from the database hbtn_0e_0_usa
+listing of states with a name starting with N from the database hbtn_0e_0_usa
 '''
 import MySQLdb
 from sys import argv
